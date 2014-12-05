d1c62f01d91f0bb4296328e1c2e788471ea9b483
Kyle Isom <kyle@cloudflare.com>
2014-12-05 12:18:32 -0800

    Formatting and inline address calls

--- src/kern/timer.s
+++ src/kern/timer.s
@@ -7,24 +7,16 @@
  */
 
 /*
- * TimerAddress returns the address of the timer.
- */
-.globl TimerAddress
-TimerAddress:
-  push  {lr}
-  ldr   r0, =0x20003000
-  pop   {pc}
-
-/*
  * Timestamp returns the current timer as a 64-bit value in r0 (low)
  * and r1 (high).
  */
 .globl Timestamp
 Timestamp:
-  push  {lr}
-  bl    TimerAddress
-  ldrd  r0, r1, [r0,#4]
-  pop   {pc}
+	push	{lr}
+	ldr	r0,	=0x20003000
+	bl	TimerAddress
+	ldrd	r0,	r1,	[r0,#4]
+	pop	{pc}
 
 /*
  * Wait takes a 32-bit delay in microseconds (stored in r0), and waits
@@ -32,15 +24,15 @@ Timestamp:
  */
 .globl Wait
 Wait:
-  mov   r2, r0
-  push  {lr}
-  bl    Timestamp
-  mov   r3, r0
+	mov	r2,	r0
+	push	{lr}
+	bl	Timestamp
+	mov	r3,	r0
 
-  waitLoop$:
-    bl  Timestamp
-    sub r1, r0, r3
-    cmp r1, r2
-    bls waitLoop$
-  
-  pop   {pc}
+waitLoop$:
+	bl	Timestamp
+	sub	r1,	r0,	r3
+	cmp	r1,	r2
+	bls	waitLoop$
+
+	pop	{pc}
